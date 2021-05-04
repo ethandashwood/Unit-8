39597c4c2f1f550b874c6d5595477ae4e117d08a
ethandashwood <72386500+ethandashwood@users.noreply.github.com>
2021-05-04 11:53:08 +0100

    Initial commit

---
+++ Assets/Scripts/Walking.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Walking : MonoBehaviour
+{
+    Rigidbody2D rb;
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+
+
+
+    void Update()
+    {
+        float xSpeed = 0.0f;
+        float ySpeed = -0.0f;
+
+
+        if (Input.GetKey("d"))
+        {
+
+            xSpeed = 8.0f;
+            transform.position += new Vector3(xSpeed * Time.deltaTime, 0, 0);
+            transform.rotation = Quaternion.Euler(0, 0f, 0);
+        }
+
+        if (Input.GetKey("a"))
+        {
+
+            xSpeed = -8.0f;
+            transform.position += new Vector3(xSpeed * Time.deltaTime, 0, 0);
+            transform.rotation = Quaternion.Euler(0, 180f, 0);
+
+        }
+    }
+}
